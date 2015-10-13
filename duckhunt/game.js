b1033374bb6b2710804929c3e6a0fcb16b524534
Joshua Berl <Josh@wr-130-64-98-56.medford.tufts.edu>
2015-10-13 14:45:11 -0400

    birds are on the background

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -3,9 +3,13 @@ function init() {
 	var ctx = canvas.getContext('2d');
 	var background = new Image();
 	background.onload = function(){
-		ctx.drawImage(background, 0, 0);
+		ctx.drawImage(background, 0, 0, 600, 800);
+		var spriteSheet = new Image();
+		spriteSheet.onload = function(){
+			ctx.drawImage(spriteSheet, 124, 114, 41, 31, 370, 100, 80, 80);
+			ctx.drawImage(spriteSheet, 42, 117, 38, 30, 300, 100, 80, 60);
+		};
+		spriteSheet.src = 'duckhunt_various_sheet.png'
 	};
 	background.src = 'duckhunt-background.gif';
-	var spriteSheet = new Image();
-	spriteSheet.src = 'duckhunt_various_sheet.png'
 }
