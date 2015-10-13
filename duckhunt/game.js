840a9c69bd6e3d62eea3586e9404905cf60b8656
Joshua Berl <Josh@wr-130-64-98-56.medford.tufts.edu>
2015-10-13 10:49:35 -0400

    background displays

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,11 @@
-// Replace this with your code...
+function init() {
+	var canvas = document.getElementById('game_canvas');
+	var ctx = canvas.getContext('2d');
+	var background = new Image();
+	background.onload = function(){
+		ctx.drawImage(background, 0, 0);
+	};
+	background.src = 'duckhunt-background.gif';
+	var spriteSheet = new Image();
+	spriteSheet.src = 'duckhunt_various_sheet.png'
+}
