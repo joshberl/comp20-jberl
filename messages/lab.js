c4b0d701eeedfa76fd700340141e79bf699f280e
Joshua Berl <Josh@wr-130-64-98-133.medford.tufts.edu>
2015-10-19 16:01:30 -0400

    fixed json source

--- messages/lab.js
+++ messages/lab.js
@@ -1,14 +1,12 @@
 function parse() {
 	var xhr = new XMLHttpRequest()
-	xhr.open('GET', '/data.json');
+	xhr.open('GET', '/messages/data.json');
 	xhr.onreadystatechange = function() {
 		if (xhr.ReadyState = 4) 
 			jsondata = JSON.parse(xhr.responseText)
-			//console.log(jsondata);
 			elem = document.getElementById("messages");
 			elem.innerHTML = ""
 			for (i = 0; i < jsondata.length; i++) {
-				//console.log(Object.keys(jsondata[i]));
 				elem.innerHTML += "<p>" + jsondata[i]["username"] + ": " + jsondata[i]["content"] + "</p>";
 			}
 	}
