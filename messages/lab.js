828d9fd484cb805205fd8e5083b9f1ba9ecb55ab
Joshua Berl <Josh@wr-130-64-98-133.medford.tufts.edu>
2015-10-19 14:23:06 -0400

    figuring out branching

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,10 @@
 // Your JavaScript goes here...
+function parse() {
+	jsondata = data.json;
+	parsedMessages = JSON.parse(jsondata);
+	elem = document.geElementByID("messages");
+	for (i = 0l i < parsedMessages.length; i++) {
+		console.log(Object.keys(parsedMessages[i]));
+		elem.innterHTML += "<p>" + parseObjects[i]["username"] + ": " parsedObjects[i]["content"] + "</p>";
+	}
+}
