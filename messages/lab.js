87c38104ad812b0d7f236bc129b9a203c095a8b3
Joshua Berl <Josh@wr-130-64-98-133.medford.tufts.edu>
2015-10-19 15:43:41 -0400

    submitting lab6

--- messages/lab.js
+++ messages/lab.js
@@ -1,9 +1,16 @@
 function parse() {
-	jsondata = data.json;
-	parsedMessages = JSON.parse(jsondata);
-	elem = document.geElementByID("messages");
-	for (i = 0l i < parsedMessages.length; i++) {
-		console.log(Object.keys(parsedMessages[i]));
-		elem.innterHTML += "<p>" + parseObjects[i]["username"] + ": " parsedObjects[i]["content"] + "</p>";
+	var xhr = new XMLHttpRequest()
+	xhr.open('GET', '/data.json');
+	xhr.onreadystatechange = function() {
+		if (xhr.ReadyState = 4) 
+			jsondata = JSON.parse(xhr.responseText)
+			//console.log(jsondata);
+			elem = document.getElementById("messages");
+			elem.innerHTML = ""
+			for (i = 0; i < jsondata.length; i++) {
+				//console.log(Object.keys(jsondata[i]));
+				elem.innerHTML += "<p>" + jsondata[i]["username"] + ": " + jsondata[i]["content"] + "</p>";
+			}
 	}
+	xhr.send();
 }
