cf141a997875fc3ad1e57eeb9e6b5cebde1a15f5
Joshua Berl <Josh@wr-130-64-98-133.medford.tufts.edu>
2015-10-21 11:40:35 -0400

    fixed console log error

--- messages/lab.js
+++ messages/lab.js
@@ -1,14 +1,17 @@
 function parse() {
 	var xhr = new XMLHttpRequest()
-	xhr.open('GET', 'http://messagehub.herokuapp.com/messages.json');
+	xhr.open('GET', 'http://messagehub.herokuapp.com/messages.json', true);
 	xhr.onreadystatechange = function() {
-		if (xhr.ReadyState = 4) 
-			jsondata = JSON.parse(xhr.responseText)
-			elem = document.getElementById("messages");
-			elem.innerHTML = ""
-			for (i = 0; i < jsondata.length; i++) {
-				elem.innerHTML += "<p>" + jsondata[i]["username"] + ": " + jsondata[i]["content"] + "</p>";
+		if (xhr.readyState == 4) {
+			if (xhr.status === 200) {
+				data = JSON.parse(xhr.responseText);
+				elem = document.getElementById("messages");
+				//elem.innerHTML = "";
+				for (i = 0; i < data.length; i++) {
+					elem.innerHTML += "<p>" + data[i]["username"] + ": " + data[i]["content"] + "</p>";
+				}
 			}
-	}
-	xhr.send();
+		}
+	};
+	xhr.send(null);
 }
