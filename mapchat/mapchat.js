8512745891705fa96b66875cc032f9517d3af060
Joshua Berl <Josh@wr-130-64-98-133.medford.tufts.edu>
2015-10-23 12:53:57 -0400

    added files from gh-pages

--- mapchat/mapchat.js
+++ mapchat/mapchat.js
@@ -3,13 +3,13 @@ var mylat = 0;
 var mylong = 0;
 var me = new google.maps.LatLng(mylat, mylong);
 var myOptions = {
-	zoom: 13.
+	zoom: 13,
 	center: me,
 	mapTypeId: google.maps.MapTypeId.ROADMAP
 };
 var map;
 var marker;
-var infowindow = new google.maps.Infowindow();
+var infowindow = new google.maps.InfoWindow();
 
 function init() {
 	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
@@ -33,11 +33,43 @@ function renderMap() {
 	map.panTo(me);
 	marker = new google.maps.Marker({
 		position: me,
-		title: "Here I am!"
+		title: "RichRumfelt",
+		icon: 'chimp.png'
 	});
 	marker.setMap(map);
 	google.maps.event.addListener(marker, 'click', function() {
 		infowindow.setContent(marker.title);
 		infowindow.open(map, marker);
 	});
+	post(mylat, mylong, "RichRumfelt")
+}
+
+function post(lat, lng, login) {
+	var url = "https://secret-about-box.herokuapp.com/sendLocation";
+	var params = "login=" + login + "&lat=" + lat + "&lng=" + lng;
+	request.open("POST", url, true);
+	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	request.send(params);
+	request.onreadystatechange = function() {
+		if (request.readyState == 4 && request.status == 200) {
+			data = JSON.parse(request.responsText);
+			for (i = 0; i < data.length; i++) {
+				new_marker(data[i], login);
+			}
+		}
+	}
+}
+
+function new_marker(data, myname) {
+	username = data["login"];
+	if (username != myname) {	
+		lat = data["lat"];
+		lng = data["lng"];
+		pos = new google.maps.LatLng(lat, lng);
+		marker = new google.maps.Marker({
+			position: pos,
+			title: username,
+			map: map
+		});
+	}
 }
